4c2dd2cc639bf6165145db6feccdfe993dbe8964
Andrea Bizzotto <codewithandrea@gmail.com>
2023-02-07 11:07:32 +0000

    Add AuthStateChangeActions and ProfileScreen

--- lib/src/screens/custom_profile_screen.dart
+++ lib/src/screens/custom_profile_screen.dart
@@ -1,15 +1,25 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomProfileScreen extends ConsumerWidget {
   const CustomProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
+    final authProviders = [EmailAuthProvider()];
+    return ProfileScreen(
       appBar: AppBar(
         title: const Text('Profile'),
       ),
+      providers: authProviders,
+      actions: [
+        SignedOutAction((context) {
+          context.goNamed(AppRoute.signIn.name);
+        }),
+      ],
     );
   }
 }
--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,6 +1,8 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
@@ -14,6 +16,14 @@ class CustomSignInScreen extends ConsumerWidget {
       ),
       body: SignInScreen(
         providers: authProviders,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+          AuthStateChangeAction<UserCreated>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+        ],
       ),
     );
   }
