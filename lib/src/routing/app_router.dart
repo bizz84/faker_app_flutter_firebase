58c00a1d26252cd552127a021b39b3139b23d4d3
Andrea Bizzotto <codewithandrea@gmail.com>
2023-02-07 14:07:03 +0000

    Add GoRouter redirect

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,6 +13,19 @@ final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+      if (isLoggedIn) {
+        if (state.location == '/sign-in') {
+          return '/profile';
+        }
+      } else {
+        if (state.location == '/profile') {
+          return '/sign-in';
+        }
+      }
+      return null;
+    },
     routes: [
       GoRoute(
         path: '/sign-in',
