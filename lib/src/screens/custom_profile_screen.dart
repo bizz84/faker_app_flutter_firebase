59c7460e7f11f42ca0f4beeb6610546e8cdff43f
Andrea Bizzotto <codewithandrea@gmail.com>
2023-02-07 14:37:04 +0000

    Add refreshListenable

--- lib/src/screens/custom_profile_screen.dart
+++ lib/src/screens/custom_profile_screen.dart
@@ -1,8 +1,6 @@
-import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class CustomProfileScreen extends ConsumerWidget {
   const CustomProfileScreen({super.key});
@@ -15,11 +13,6 @@ class CustomProfileScreen extends ConsumerWidget {
         title: const Text('Profile'),
       ),
       providers: authProviders,
-      actions: [
-        SignedOutAction((context) {
-          context.goNamed(AppRoute.signIn.name);
-        }),
-      ],
     );
   }
 }
