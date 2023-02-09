d806cfd149055868845403a3f790e1241391619c
Andrea Bizzotto <codewithandrea@gmail.com>
2023-02-09 12:12:29 +0000

    Add basic FirestoreRepository class

---
+++ lib/src/data/firestore_repository.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FirestoreRepository {
+  FirestoreRepository(this._firestore);
+  final FirebaseFirestore _firestore;
+
+  Future<void> addJob(String uid, String title, String company) async {
+    final docRef = await _firestore.collection('jobs').add({
+      'uid': uid,
+      'title': title,
+      'company': company,
+    });
+    debugPrint(docRef.id);
+  }
+}
+
+final firestoreRepositoryProvider = Provider<FirestoreRepository>((ref) {
+  return FirestoreRepository(FirebaseFirestore.instance);
+});
