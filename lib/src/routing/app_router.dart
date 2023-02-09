943cd96da4675461adc92bdcda580914d28af38b
Andrea Bizzotto <codewithandrea@gmail.com>
2023-02-09 11:19:18 +0000

    Update routes
    # Conflicts:
    #	lib/src/routing/app_router.dart

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,12 +1,14 @@
 import 'package:faker_app_flutter_firebase/src/routing/go_router_refresh_stream.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
 import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:faker_app_flutter_firebase/src/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 enum AppRoute {
   signIn,
+  home,
   profile,
 }
 
@@ -23,10 +25,10 @@ final goRouterProvider = Provider<GoRouter>((ref) {
       final isLoggedIn = firebaseAuth.currentUser != null;
       if (isLoggedIn) {
         if (state.uri.path == '/sign-in') {
-          return '/profile';
+          return '/home';
         }
       } else {
-        if (state.uri.path == '/profile') {
+        if (state.uri.path.startsWith('/home')) {
           return '/sign-in';
         }
       }
@@ -40,9 +42,16 @@ final goRouterProvider = Provider<GoRouter>((ref) {
         builder: (context, state) => const CustomSignInScreen(),
       ),
       GoRoute(
-        path: '/profile',
-        name: AppRoute.profile.name,
-        builder: (context, state) => const CustomProfileScreen(),
+        path: '/home',
+        name: AppRoute.home.name,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: 'profile',
+            name: AppRoute.profile.name,
+            builder: (context, state) => const CustomProfileScreen(),
+          ),
+        ],
       ),
     ],
   );
