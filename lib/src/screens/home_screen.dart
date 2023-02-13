cb552bfe37c0972585d80a2bcd24d887c6a4a619
Andrea Bizzotto <codewithandrea@gmail.com>
2023-02-13 11:09:09 +0000

    Added JobsListView, firebase_ui_firestore
    # Conflicts:
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	pubspec.lock
    
    # Conflicts:
    #	pubspec.lock
    
    # Conflicts:
    #	pubspec.lock
    #	pubspec.yaml
    
    # Conflicts:
    #	pubspec.lock

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker/faker.dart';
 import 'package:faker_app_flutter_firebase/src/data/firestore_repository.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -17,6 +19,7 @@ class HomeScreen extends ConsumerWidget {
           onPressed: () => context.goNamed(AppRoute.profile.name),
         )
       ]),
+      body: const JobsListView(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
@@ -31,6 +34,25 @@ class HomeScreen extends ConsumerWidget {
               );
         },
       ),
+    );
+  }
+}
+
+class JobsListView extends ConsumerWidget {
+  const JobsListView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    return FirestoreListView(
+      query: firestoreRepository.jobsQuery(),
+      itemBuilder: (BuildContext context,
+          QueryDocumentSnapshot<Map<String, dynamic>> doc) {
+        return ListTile(
+          title: Text(doc['title']),
+          subtitle: Text(doc['company']),
+        );
+      },
     );
   }
 }
