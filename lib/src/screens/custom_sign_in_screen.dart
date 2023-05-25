694bfcc7664068132b59981b6a04808ba58aa0db
Andrea Bizzotto <codewithandrea@gmail.com>
2023-05-25 10:04:07 +0100

    Install firebase_auth, firebase_ui_auth, ios & Android project setup, update CustomSignInScreen

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,9 +7,13 @@ class CustomSignInScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authProviders = [EmailAuthProvider()];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sign in'),
+      ),
+      body: SignInScreen(
+        providers: authProviders,
       ),
     );
   }
