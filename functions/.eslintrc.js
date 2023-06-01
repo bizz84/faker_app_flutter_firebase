16a908bfbf953c9efa69648626f5f32a1a99cdf3
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-01 10:37:55 +0100

    Updated cloud functions

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,6 +28,16 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    "indent": ["off"],
+    "semi": ["off"],
+    "require-jsdoc": ["off"],
+    "object-curly-spacing": ["off"],
+    "spaced-comment": ["off"],
+    "max-len": [
+      "error",
+      {
+        "code": 140,
+      },
+    ],
   },
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,64 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import * as admin from "firebase-admin"
+import * as functions from "firebase-functions/v2"
+
+//import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// https://github.com/firebase/firebase-tools/issues/1532
+if (admin.apps.length === 0) {
+  admin.initializeApp()
+}
 
-// export const helloWorld = onRequest((request, response) => {
+// export const helloWorld = functions.https.onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const makeJobUppercase = functions.firestore.onDocumentWritten("/users/{uid}/jobs/{jobId}", (e) => {
+  const change = e.data;
+  if (change === undefined) {
+    return
+  }
+  const data = change.after.data()
+  if (data === undefined) {
+    // If the document has been deleted, do nothing
+    return
+  }
+  const uppercase = data.title.toUpperCase()
+  // If the title is already uppercase, do nothing
+  if (uppercase == data.title) {
+    logger.log(`Skipping /users/${e.params.uid}/jobs/${e.params.jobId}: ${data.title}`)
+    return
+  }
+  // else, make uppercase and update document
+  logger.log(`Uppercasing /users/${e.params.uid}/jobs/${e.params.jobId}: ${data.title} > ${uppercase}`)
+  return change.after.ref.set({title: uppercase}, {merge: true})
+})
+
+
+export const deleteAllUserJobs = functions.https.onCall(async (context: functions.https.CallableRequest) => {
+  const uid = context.auth?.uid
+  if (uid === undefined) {
+    // Show how strong TS type safety is
+    throw new functions.https.HttpsError("unauthenticated", "You need to be authenticated to perform this action")
+  }
+  const firestore = admin.firestore()
+  const collectionRef = firestore.collection(`/users/${uid}/jobs`)
+  const collection = await collectionRef.get()
+  logger.debug(`Deleting ${collection.docs.length} docs at "/users/${uid}/jobs"`)
+  // transaction version
+  await firestore.runTransaction(async (transaction) => {
+      for (const doc of collection.docs) {
+          transaction.delete(firestore.doc(`/users/${uid}/jobs/${doc.id}`))
+      }
+  })
+  // normal version
+  // for (const doc of collection.docs) {
+  //     await firestore.doc(`/users/${uid}/jobs/${doc.id}`).delete()
+  // }
+  logger.debug(`Deleted ${collection.docs.length} docs at "/users/${uid}/jobs"`)
+  return {"success": true}
+})
