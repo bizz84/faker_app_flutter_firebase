b7adbcfd98a0f891aede7911e3ac22a43643ccb7
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-07 14:35:33 +0100

    Connect the Flutter app to the Firebase emulator, Android cleartext HTTP traffic fix
    # Conflicts:
    #	android/app/src/main/AndroidManifest.xml

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker_app_flutter_firebase/firebase_options.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+Future<void> setupEmulators() async {
+  await FirebaseAuth.instance.useAuthEmulator('127.0.0.1', 9099);
+  FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await setupEmulators();
   runApp(const ProviderScope(child: MyApp()));
 }
 
