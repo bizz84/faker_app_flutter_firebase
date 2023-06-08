dfd21797bff1659b0aca3af0954f1d4dc27ee763
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-08 10:56:02 +0100

    Uncomment helloWorld function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -13,7 +13,7 @@ import * as logger from "firebase-functions/logger";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
