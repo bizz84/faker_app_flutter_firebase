176ac3eac52026fa34f922f239c0a4667f36bed7
Andrea Bizzotto <codewithandrea@gmail.com>
2023-06-09 11:18:49 +0100

    Add deleteAllUserJobs function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,3 +23,18 @@ export const makeJobTitleUppercase = functions.firestore.onDocumentWritten("/use
   logger.log(`Uppercasing ${change.after.ref.path}: ${data.title} => ${uppercase}`)
   return change.after.ref.set({title: uppercase}, {merge: true})
 })
+
+export const deleteAllUserJobs = functions.https.onCall(async (context: functions.https.CallableRequest) => {
+  const uid = context.auth?.uid
+  if (uid === undefined) {
+    throw new functions.https.HttpsError("unauthenticated", "You need to be authenticated to perform this action")
+  }
+  const firestore = admin.firestore()
+  const collectionRef = firestore.collection(`/users/${uid}/jobs`)
+  const docRefs = await collectionRef.listDocuments()
+  for (const docRef of docRefs) {
+    await docRef.delete()
+  }
+  logger.log(`Deleted ${docRefs.length} docs at ${collectionRef.path}`)
+  return {"success": true}
+})
