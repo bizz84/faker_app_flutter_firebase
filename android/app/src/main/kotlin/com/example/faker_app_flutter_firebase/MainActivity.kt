99e7f7eb39072ba85935017f96185cbb38463e11
Andrea Bizzotto <codewithandrea@gmail.com>
2024-05-16 08:47:50 +0100

    Recreate android, ios, macos, web projects with Flutter 3.22

--- android/app/src/main/kotlin/com/example/faker_app_flutter_firebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/faker_app_flutter_firebase/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.faker_app_flutter_firebase
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
