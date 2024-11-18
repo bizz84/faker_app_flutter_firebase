c968b20cc37b70370bf4b20a32e2102539f8c249
Andrea Bizzotto <codewithandrea@gmail.com>
2024-11-18 10:35:29 +0000

    Run `flutterfire configure`, add firebase_core 3.8.0, update minimum ios & osx version in Podfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:faker_app_flutter_firebase/firebase_options.dart';
 import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
